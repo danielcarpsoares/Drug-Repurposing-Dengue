567bdeea4dc02a6205e52a4bde08753156cc10c0
danielcarpsoares <50836106+danielcarpsoares@users.noreply.github.com>
2019-05-22 10:40:54 +0100

    Add files via upload

---
+++ Differential Expression Evaluation in Array Data/GSE18090.R
@@ -0,0 +1,47 @@
+################################################################
+###   Differential expression analysis with limma
+library(GEOquery)
+library(limma)
+library(genefilter)
+library(hgu133plus2.db)
+
+###Load series and platform data from GEO
+gset <- getGEO("GSE18090", GSEMatrix =TRUE, AnnotGPL=TRUE)
+if (length(gset) > 1) idx <- grep("GPL570", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+###Remove duplicated probes
+gset@annotation = "hgu133plus2"
+gset <- featureFilter(gset, require.entrez = T, remove.dupEntrez = T)
+
+###Add metadata to dataset 
+fvarLabels(gset) <- make.names(fvarLabels(gset))
+
+###Group names for all samples: x-remove; 1-infected; 0-controls
+gsms <- "XXXXXXXX111111111100000000"
+sml <- c()
+for (i in 1:nchar(gsms)) { sml[i] <- substr(gsms,i,i) }
+
+###Eliminate samples marked as "x"
+sel <- which(sml != "X")
+sml <- sml[sel]
+gset <- gset[ ,sel]
+
+###Log2 transform
+exprs(gset) <- log2(exprs(gset))
+
+###Set up the data and proceed with analysis
+sml <- paste("G", sml, sep="")    # set group names
+fl <- as.factor(sml)
+gset$description <- fl
+design <- model.matrix(~ description + 0, gset)
+colnames(design) <- levels(fl)
+fit <- lmFit(gset, design)
+cont.matrix <- makeContrasts(G1-G0, levels=design)
+fit2 <- contrasts.fit(fit, cont.matrix)
+fit2 <- eBayes(fit2, 0.01)
+
+tT <- topTable(fit2, adjust="fdr", sort.by="B", number=22000)
+
+tT <- subset(tT, select=c("ID","adj.P.Val","P.Value","logFC","Gene.symbol","Gene.title"))
+write.table(tT, file="genes18090deftest.txt", row.names=F, sep="\t")
